ee4638c00b231530249cc284517a9c21917f2720
FURUSHKA <163753122+FYURi4@users.noreply.github.com>
2024-11-29 11:48:31 +0300

    Update High_panel.js

--- JavaScript/High_panel.js
+++ JavaScript/High_panel.js
@@ -1,32 +1,35 @@
 function toggleHighPanel() {
     var highPanel = document.querySelector('.high_panel');
-    var dateDisplay = document.getElementById("dateDisplay"); // Получаем элемент даты
+    var hpInfo = document.querySelector('.hp_info');
+    var dateDisplay = document.getElementById("dateDisplay");
+    var hpMicoContainer = document.getElementById("hp_mico_container");
     var clock = document.getElementById("clock");
-    var hpAlarmContainer = document.getElementById("hp_alarm_container");
 
     // Проверяем текущее состояние панели
     if (highPanel.style.height === '578px') {
         // Закрытие панели
         highPanel.style.borderRadius = '25px 25px 0px 0px'; 
-        highPanel.style.height = '23px'; // Закрыть панель
+        highPanel.style.height = '23px';
+        hpInfo.style.top = "4px";
         // Настройка часов
-        clock.style.top = "0px"; 
+        clock.style.top = "1px"; 
         clock.style.left = "26px"; 
         clock.style.fontSize = "20px"; 
         // Скрыть дату
         dateDisplay.style.display = 'none';
-        hpAlarmContainer.style.display = 'none';
+        hpMicoContainer.style.display = 'none';
+
     } else {
         // Открытие панели
         highPanel.style.borderRadius = '25px'; 
         highPanel.style.height = '578px'; // Открыть панель на весь экран
-        highPanel.style.backdropFilter = 'blur(5px)';
+        hpInfo.style.top = "20px";
         // Настройка часов
-        clock.style.top = "30px"; 
+        clock.style.top = "40px"; 
         clock.style.left = "30px"; 
         clock.style.fontSize = "43px"; 
         // Показать дату
         dateDisplay.style.display = 'block';
-        hpAlarmContainer.style.display = 'block';
+        hpMicoContainer.style.display = 'block';
     }
 }
